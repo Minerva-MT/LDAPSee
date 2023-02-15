001c7ea188c7cc30419c5bbe287c23857c4aee19
Minerva-MT <95608312+Minerva-MT@users.noreply.github.com>
2023-02-15 09:24:42 +0100

    Add files via upload

--- Source/LDAP.c
+++ Source/LDAP.c
@@ -40,91 +40,154 @@ int LDAP_Bind (LDAP ** LDAPConnection, char * Username, char * Password)
 }
 
 void LDAP_Enumerate (LDAP ** LDAPConnection, char * BaseDN, char * Filter, char * Attributes[])
-{
-    Verbose("LDAP Query: %s\n\n", Filter);
-    
+{    
     LDAPMessage * Answer = LDAP_Search (LDAPConnection, LDAP_SCOPE_SUBTREE, BaseDN, Filter, Attributes);
-    
-    ParseAnswer(LDAPConnection, Answer);
-}
-
-void ParseAnswer(LDAP ** LDAPConnection, LDAPMessage * Answer)
-{
-        
-    LDAPMessage     *   Entry;
-    
-    char            *   Attribute;  
-    
-    BerVarray       *   Values;
-    
-    BerElement      *   AttributePointer;
-    
-    Entry           =   ldap_first_entry(*LDAPConnection, Answer);
-
-    if(ldap_count_entries(*LDAPConnection, Answer) == 0)
-    {
-        printf("\t No entries were found\n");
-        return;
-    }
-    
-    while (Entry)
-    {
-        char * DN = ldap_get_dn(*LDAPConnection, Entry);
-
-        if (DN[0] != 0)
-            printf("\t DN: %s\n", DN);
-        
-        ldap_memfree(DN);
-        
-        Attribute = ldap_first_attribute(*LDAPConnection, Entry, &AttributePointer);
-        
-        while (Attribute)
-        {
-            Values = ldap_get_values_len(*LDAPConnection, Entry, Attribute);
-            
-            for (int i = 0; i < ldap_count_values_len(Values); i++)
-                printf("\t\t %s: %s\n", Attribute, Values[i] -> bv_val);
-                
-            ldap_value_free_len(Values);
-            
-            Attribute = ldap_next_attribute(*LDAPConnection, Entry, AttributePointer);  
-        }
-        
-        ber_free(AttributePointer, 0);
-        
-        LDAPMessage * NextEntry = ldap_next_entry(*LDAPConnection, Entry);
-
-        ldap_memfree(Entry);
-
-        Entry = NextEntry;
-        
-    }
-    
 }
 
 LDAPMessage * LDAP_Search (LDAP ** LDAPConnection, int SearchScope, char * BaseDN, char * Filter, char * Attributes[])
 {
-    struct timeval Timeout;
-    Timeout.tv_sec = 100;
-    Timeout.tv_usec = 0;
-    
     LDAPMessage *   Answer;
+    
+    struct berval * Cookie              =   NULL;
+    
+	LDAPControl *   PageControl         =   NULL;
+	LDAPControl *   ServerControls[2]   =   {NULL, NULL};
+	LDAPControl **  ReturnedControls    =   NULL;
+	
+	char            PagingCriticality   =   'T';
+	
+	int             RetCode             =   0;
+	
+	int             ErrorCode           =   0;
+	
+	char            MorePages           =   0;
+	
+	int             TotalResults        =   0;
+	
+	do
+	{
+	    Verbose("Creating a Page Control Structure of size %d\n", PAGE_SIZE);
+	    
+	    RetCode = ldap_create_page_control (
+	        *LDAPConnection,
+	        PAGE_SIZE,
+	        Cookie,
+	        1,
+	        &PageControl
+        );
         
-    int RetCode = ldap_search_ext_s(
-        *LDAPConnection, 
-        BaseDN, 
-        SearchScope, 
-        Filter, 
-        Attributes, 
-        0, 
-        NULL, 
-        NULL, 
-        &Timeout, 
-        SIZE_LIMIT, 
-        &Answer
-    );
-    
-    ErrorCheck(RetCode, "Error Searching LDAP");
-    
-    return Answer;
+        ServerControls[0] = PageControl;
+        
+        
+        Verbose("Performing Query: %s \n", Filter);
+        
+        RetCode = ldap_search_ext_s(
+            *LDAPConnection, 
+            BaseDN, 
+            SearchScope, 
+            Filter, 
+            Attributes, 
+            0, 
+            ServerControls, 
+            NULL, 
+            NULL, 
+            0, 
+            &Answer
+        );
+        
+        if ((RetCode != LDAP_SUCCESS) && (RetCode != LDAP_PARTIAL_RESULTS))
+        {
+            printf("Error Searching LDAP\n");
+        }
+        
+        RetCode = ldap_parse_result(
+            *LDAPConnection,
+            Answer,
+            &ErrorCode,
+            NULL,
+            NULL,
+            NULL,
+            &ReturnedControls,
+            0
+        );
+        
+        ErrorCheck (RetCode, "Error Parsing Result");
+        
+        if (Cookie != NULL && Cookie -> bv_val != NULL)
+        {
+            ber_bvfree(Cookie);
+            Cookie = NULL;
+        }
+        
+        int RecordsReturned = 0;
+        
+        RetCode = ldap_parse_page_control(
+            *LDAPConnection,
+            ReturnedControls,
+            &RecordsReturned,
+            &Cookie
+        );
+        
+        
+        if (Cookie && Cookie -> bv_val != NULL && (strlen(Cookie -> bv_val) > 0))
+            MorePages = 1;
+        else
+            MorePages = 0;
+            
+        if (ReturnedControls != NULL)
+        {
+            ldap_controls_free(ReturnedControls);
+            ReturnedControls = NULL;
+        }
+        
+        ServerControls[0] = NULL;
+        
+        ldap_control_free (PageControl);
+        
+        PageControl = NULL;
+        
+        LDAPMessage * Entry = NULL;
+        
+        char            *   Attribute;  
+        BerVarray       *   Values;
+        
+        for (
+            Entry = ldap_first_entry(*LDAPConnection, Answer); 
+            Entry != NULL; 
+            Entry = ldap_next_entry(*LDAPConnection, Entry))
+        {
+            char * DN = ldap_get_dn(*LDAPConnection, Entry);
+            BerElement      *   AttributePointer;
+            
+            if (DN[0] != 0)
+                printf("\t DN: %s\n", DN);
+            
+            ldap_memfree(DN);
+            
+            Attribute = ldap_first_attribute(*LDAPConnection, Entry, &AttributePointer);
+            
+            while (Attribute)
+            {
+                Values = ldap_get_values_len(*LDAPConnection, Entry, Attribute);
+                
+                for (int i = 0; i < ldap_count_values_len(Values); i++)
+                    printf("\t\t %s: %s\n", Attribute, Values[i] -> bv_val);
+                    
+                ldap_value_free_len(Values);
+                
+                Attribute = ldap_next_attribute(*LDAPConnection, Entry, AttributePointer);  
+            }
+            
+			printf("\n");
+
+            TotalResults ++;
+            
+            ber_free(AttributePointer, 0);
+        
+        }
+        
+        ldap_msgfree(Answer);
+          
+    } while (MorePages);
+
 }
--- Source/Modules.c
+++ Source/Modules.c
@@ -1,5 +1,17 @@
 #include "../Headers/Modules.h"
 
+char * UserAttributes[] = 
+{ 
+    "cn", 
+	"objectCategory",
+    "SAMaccountName", 
+    "UserPrincipalName", 
+    "Description", 
+    "Badpwdcount", 
+    "servicePrincipalName",
+    NULL 
+};
+
 int ENUM_Context(LDAP ** LDAPConnection)
 {
     BerValue * Authorisation;
@@ -22,20 +34,27 @@ void ENUM_Users (LDAP ** LDAPConnection, char * BaseDN)
 {
     char * Filter       =   "(&(objectClass=User)(objectCategory=Person))";
     
-    char * Attributes[] = { "cn", "sAMACcountName", "userPrincipalName", "description", "badpwdcount", NULL };
+    printf("\n[+] Enumerating Users: \n");
     
-    printf("[+] Enumerating Users: \n");
-    
-    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes); 
+    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, UserAttributes); 
 }
 
+void ENUM_PasswordExpiry (LDAP ** LDAPConnection, char * BaseDN)
+{
+	char * Filter		=	"(&(objectCategory=Person)(userAccountControl:1.2.840.113556.1.4.803:=65536))";
+
+    printf("\n[+] Enumerating Users Accounts which never Expire: \n");
+    
+    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, UserAttributes); 
+
+}
 void ENUM_Groups (LDAP ** LDAPConnection, char * BaseDN)
 {
     char * Filter       =   "(objectClass=group)";
     
     char * Attributes[] = { "cn", "sAMACcountName", NULL };
     
-    printf("[+] Enumerating Groups: \n");
+    printf("\n[+] Enumerating Groups: \n");
     
     LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes); 
 }
@@ -46,18 +65,36 @@ void ENUM_Computers (LDAP ** LDAPConnection, char * BaseDN)
     
     char * Attributes[]         =   { "operatingSystem", "operatingSystemVersion", "dNSHostName", "msDS-KeyCredentialLink", "description", NULL };
     
-    printf("[+] Enumerating Computers: \n");
+    printf("\n[+] Enumerating Computers: \n");
     
     LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes); 
 }
 
 void ENUM_Delegation(LDAP ** LDAPConnection, char * BaseDN)
 {
-    char * Filter               =   "(&(|(UserAccountControl:1.2.840.113556.1.4.803:=16777216)(UserAccountControl:1.2.840.113556.1.4.803:=524288)(msDS-AllowedToDelegateTo=*)(msDS-AllowedToActOnBehalfOfOtherIdentity=*))(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(!(UserAccountControl:1.2.840.113556.1.4.803:=8192)))";
+	/*
+		Query Description:
+			User Account Control Value 16777216 - Check for Trusted to Authenticated for Delegation User Accounts 	- TRUSTED_TO_AUTH_FOR_DELEGATION
+			User Account Control Value 524288   - Check for Unconstrained Delegation User Accounts 					- TRUSTED_FOR_DELEGATION
+			msDS-Allowed-To-Delegate-To         - Configures a service so that it can obtain Constrained Delegation Service Tickets
+			msDS-AllowedToActOnBehalfOfOtherIdentity - Determines if a requestor has permission to act on behalf of other identities
+	*/
+		
+    char * Filter               =   
+		"(&"
+			"(|"
+				"(UserAccountControl:1.2.840.113556.1.4.803:=16777216)"
+				"(UserAccountControl:1.2.840.113556.1.4.803:=524288)"
+				"(msDS-AllowedToDelegateTo=*)"
+				"(msDS-AllowedToActOnBehalfOfOtherIdentity=*)"
+			")"
+			"(!(UserAccountControl:1.2.840.113556.1.4.803:=2))"
+			"(!(UserAccountControl:1.2.840.113556.1.4.803:=8192))"
+		")";
     
     char * Attributes[]         =   { "operatingSystem", "operatingSystemVersion", "dNSHostName", "objectCategory", "msDS-AllowedToActOnBehalfOfOtherIdentity", "msDS-AllowedToDelegateTo", NULL };
     
-    printf("[+] Enumerating Unconstrained Delegation: \n");
+    printf("\n[+] Enumerating Delegations: \n");
     
     LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes); 
 }
@@ -68,7 +105,7 @@ void ENUM_POSIX (LDAP ** LDAPConnection, char * BaseDN)
        
     char * Attributes[]         =   { "sshPublicKey", NULL };
     
-    printf("[+] Enumerating POSIX Users: \n");
+    printf("\n[+] Enumerating POSIX Users: \n");
     
     LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
 }
@@ -79,7 +116,7 @@ void ENUM_GPO (LDAP ** LDAPConnection, char * BaseDN)
         
     char * Attributes[]         =   { "displayName", "gPCFileSysPath", NULL };
     
-    printf("[+] Enumerating Group Policies: \n");
+    printf("\n[+] Enumerating Group Policies: \n");
     
     LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
 }
@@ -88,22 +125,18 @@ void ENUM_SPNs (LDAP ** LDAPConnection, char * BaseDN)
 {
     char * Filter               =   "(&(servicePrincipalName=*)(UserAccountControl:1.2.840.113556.1.4.803:=512)(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(!(objectCategory=computer)))";
     
-    char * Attributes[]         =   { "cn", "sAMAccountName", "userPrincipalName", NULL };
+    printf("\n[+] Enumerating Service Principal Names: \n");
     
-    printf("[+] Enumerating Service Principal Names: \n");
-    
-    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
+    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, UserAttributes);
 }
 
 void ENUM_ASREP (LDAP ** LDAPConnection, char * BaseDN)
 {
     char * Filter               =   "(&(objectClass=user)(userAccountControl:1.2.840.113556.1.4.803:=4194304))";
     
-    char * Attributes[]         =   { "cn", "sAMAccountName", "userPrincipalName", NULL };
+    printf("\n[+] Enumerating User Accounts not Required to Pre-Authenticate: \n");
     
-    printf("[+] Enumerating User Accounts not Required to Pre-Authenticate: \n");
-    
-    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
+    LDAP_Enumerate(LDAPConnection, BaseDN, Filter, UserAttributes);
 }
 
 void ENUM_AdminObjects (LDAP ** LDAPConnection, char * BaseDN)
@@ -112,72 +145,45 @@ void ENUM_AdminObjects (LDAP ** LDAPConnection, char * BaseDN)
     
     char * Attributes[]         =   { "cn", NULL };
     
-    printf("[+] Enumerating Admin Objects: \n");
+    printf("\n[+] Enumerating Admin Objects: \n");
     
     LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
 }
 
-void ENUM_DomainAdmins (LDAP ** LDAPConnection, char * BaseDN)
+void ENUM_PrivilegedUsers (LDAP ** LDAPConnection, char * BaseDN)
 {
-    char * AdminGroups[]  =   {
-        "Domain Admins",
-	    "Domain-Admins",
-	    "Domain Administrators",
-	    "Domain-Administrators",
-	    "Domänen Admins",
-	    "Domänen-Admins",
-	    "Domain Admins",
-	    "Domain-Admins",
-	    "Domänen Administratoren",
-	    "Domänen-Administratoren",
-	    NULL
+    char  * AdminGroups[]        =   {
+	   "Domain Admins",  "Administrators", "Enterprise Admins", "Schema Admins", "Account Operators", "Backup Operators", "Server Management" , NULL
     };
     
     char Filter [250];
-            
-    char * Attributes[]         =   { "cn", NULL };
-    char ** ArrayPointer        =   AdminGroups;
     
-    for (char * AdminGroup = *ArrayPointer; AdminGroup; AdminGroup = *++ArrayPointer)
+    for (char ** ArrayPointer = AdminGroups; *ArrayPointer; ArrayPointer++)
     {
-        printf("[+] Enumerating Domain Admin Group: %s: \n", AdminGroup);
+        printf("\n[+] Enumerating Privileged Users Group: %s\n", *ArrayPointer);
         
-        snprintf(Filter, 250, "(&(objectClass=user)(|(memberof:1.2.840.113556.1.4.1941:=CN=%s,CN=Users,%s)))", AdminGroup, BaseDN);
-        LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
+        snprintf(Filter, 250, "(&(objectClass=user)(|(memberof:1.2.840.113556.1.4.1941:=CN=%s,CN=Users,%s)))", *ArrayPointer, BaseDN);
+        LDAP_Enumerate(LDAPConnection, BaseDN, Filter, UserAttributes);
     }
 }
 
-void ENUM_PrivilegedUsers (LDAP ** LDAPConnection, char * BaseDN)
+void ENUM_AdminCount (LDAP ** LDAPConnection)
 {
-    char * AdminGroups[]        =   {
-	    "Administrators", "Enterprise Admins", "Schema Admins", "Account Operators", "Backup Operators", "Server Management", "Konten-Operatoren", "Sicherungs-Operatoren", "Server-Operatoren", "Schema-Admins", NULL
-    };
+    printf("\n[+] Enumerating Admins: \n");
     
-    char Filter [250];
-        
-    char * Attributes[]         =   { "cn", NULL };
-    
-    char ** ArrayPointer        =   AdminGroups;
-    
-    for (char * AdminGroup = *ArrayPointer; AdminGroup; AdminGroup = *++ArrayPointer)
-    {
-        printf("[+] Enumerating Privileged Users Group: %s\n", AdminGroup);
-        
-        snprintf(Filter, 250, "(&(objectClass=user)(|(memberof:1.2.840.113556.1.4.1941:=CN=%s,CN=Users,%s)))", AdminGroup, BaseDN);
-        LDAP_Enumerate(LDAPConnection, BaseDN, Filter, Attributes);
-    }
+    char * Filter               =   "(&(objectClass=*)(adminCount=1))";
+
+    LDAPMessage * Answer = LDAP_Search (LDAPConnection, LDAP_SCOPE_BASE, "", Filter, UserAttributes);
 }
 
 void ENUM_Base (LDAP ** LDAPConnection)
 {
-    printf("[+] Enumerating Metadata: \n");
+    printf("\n[+] Enumerating Metadata: \n");
     
     char * Filter               =   "(objectClass=*)";
     
     char * Attributes[]         =   { "defaultNamingContext", "domainFunctionality", "forestFunctionality", "domainControllerFunctionality", "dnsHostName", NULL };
-        
+
     LDAPMessage * Answer = LDAP_Search (LDAPConnection, LDAP_SCOPE_BASE, "", Filter, Attributes);
-    
-    ParseAnswer(LDAPConnection, Answer);
 }
 
